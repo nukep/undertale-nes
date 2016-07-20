6c596c71d0557553822c7f0a2d01f89eb4fce007
Danny Spencer <dan@atomicpotato.net>
2016-07-19 21:11:44 -0700

    Turn Lesser Dog's head into sprites

--- src/animate_lesser_dog.asm
+++ src/animate_lesser_dog.asm
@@ -21,6 +21,7 @@ animate_lesser_dog:
   lda #24
   sta GENVAR0
 -
+  jsr draw_lesser_dog_sprites
   jsr yield
   dec GENVAR0
   bne -
@@ -30,8 +31,54 @@ animate_lesser_dog:
   lda #24
   sta GENVAR0
 -
+  jsr draw_lesser_dog_sprites
   jsr yield
   dec GENVAR0
   bne -
 
   beq animate_lesser_dog
+
+macro draw_lesser_dog_sprites.tile index,offx,offy,tile
+  B=OAM+(index*4)
+  COLOR=1
+  lda draw_lesser_dog_sprites.y
+  clc
+  adc #(offy-1)
+  sta B,x
+  lda #tile
+  sta B+1,x
+  lda #COLOR
+  sta B+2,x
+  lda draw_lesser_dog_sprites.x
+  if offx != 0
+    clc
+    adc #(offx)
+  endif
+  sta B+3,x
+endm
+
+draw_lesser_dog_sprites.x=TEMP0
+draw_lesser_dog_sprites.y=TEMP1
+draw_lesser_dog_sprites:
+  ; hard code the x and y coordinates for now
+  lda #(8*15+3)
+  sta draw_lesser_dog_sprites.x
+  lda #(8*5-3)
+  sta draw_lesser_dog_sprites.y
+
+  ldx OAM_SIZE
+  txa
+  clc
+  adc #(4*8)
+  sta OAM_SIZE
+
+  draw_lesser_dog_sprites.tile 0,0,0,undertale_b.chr_47
+  draw_lesser_dog_sprites.tile 1,8,0,undertale_b.chr_48
+  draw_lesser_dog_sprites.tile 2,16,0,undertale_b.chr_49
+  draw_lesser_dog_sprites.tile 3,0,8,undertale_b.chr_57
+  draw_lesser_dog_sprites.tile 4,8,8,undertale_b.chr_58
+  draw_lesser_dog_sprites.tile 5,16,8,undertale_b.chr_59
+  draw_lesser_dog_sprites.tile 6,8,16,undertale_b.chr_68
+  draw_lesser_dog_sprites.tile 7,16,16,undertale_b.chr_69
+
+  rts
